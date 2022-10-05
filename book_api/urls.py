ecb00d6eed0808ecb95c0452dfba9c6ada88fe89
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2022-10-05 11:14:57 -0300

    Complete refactor to Class Based Views

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 #from .views import book_list, book_create, book
-from .views import BookList
+from .views import BookCreate, BookList
 
 urlpatterns = [
-    #path('', book_create),
+    path('', BookCreate.as_view()),
     path('list/', BookList.as_view()),
     #path('<int:pk>', book)
 ]
--- book_api/views.py
+++ book_api/views.py
@@ -1,57 +1,10 @@
-# from django.shortcuts import render
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
-# from rest_framework import status
-# from django.core.exceptions import ObjectDoesNotExist
-# from .models import Book
-# from .serializer import BookSerializer
-#
-#
-# @api_view(['GET'])
-# def book_list(request):
-#     books = Book.objects.all()
-#     serializer = BookSerializer(books, many=True)
-#     return Response(serializer.data)
-#
-#
-# @api_view(['POST'])
-# def book_create(request):
-#     serializer = BookSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     else:
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def book(request, pk):
-#     try:
-#         book = Book.objects.get(pk=pk)
-#     except ObjectDoesNotExist:
-#         return Response({
-#             'error': 'book does not exist'
-#         }, status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == 'GET':
-#         serializer = BookSerializer(book)
-#         return Response(serializer.data)
-#
-#     if request.method == 'PUT':
-#         serializer = BookSerializer(book, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     if request.method == 'DELETE':
-#         book.delete()
-#         return Response(status.HTTP_204_NO_CONTENT)
-
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Book
 from .serializer import BookSerializer
-from rest_framework.response import Response
 
 
 class BookList(APIView):
@@ -60,9 +13,40 @@ class BookList(APIView):
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
 
+
+class BookCreate(APIView):
     def post(self, request):
-        return Response(
-            {
-                'hello': 'friend'
-            }
-        )
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Book(APIView):
+    def get_book_by_pk(self, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return Response({
+                'error': 'book does not exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        book = self.get_book_by_pk(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
